9841a7ea2f420641d3da350095d1c585a917c8f3
supermars01 <kings_821113@163.com>
2017-06-07 12:15:19 +0800

    index修改

--- js/language.js
+++ js/language.js
@@ -112,7 +112,7 @@ var execI18n = function(){
          */
         jQuery.i18n.properties({
             name : sourceName, //资源文件名称
-            path : 'i18n/' + i18nLanguage +'/', //资源文件路径
+            path : '../i18n/' + i18nLanguage +'/', //资源文件路径
             mode : 'map', //用Map的方式使用资源文件中的值
             language : i18nLanguage,
             callback : function() {//加载成功后设置显示内容
@@ -120,15 +120,6 @@ var execI18n = function(){
                 insertEle.each(function() {
                     // 根据i18n元素的 name 获取内容写入
                     $(this).html($.i18n.prop($(this).attr('name')));
-                });
-
-                var insertInputEle = $(".i18n-input");
-                insertInputEle.each(function() {
-                    var selectAttr = $(this).attr('selectattr');
-                    if (!selectAttr) {
-                        selectAttr = "value";
-                    };
-                    $(this).attr(selectAttr, $.i18n.prop($(this).attr('selectname')));
                 });
             }
         });
@@ -141,7 +132,7 @@ $(function(){
     execI18n();
 
     /*将语言选择默认选中缓存中的值*/
-    $(".lang-downlist li[data-value="+i18nLanguage+"]").addClass("current").siblings().removeClass("current")
+    $(".lang-downlist li[data-value="+i18nLanguage+"]").addClass("current").siblings().removeClass("current");
 
     /* 选择语言 */
     // $(".lang-downlist li").on('change', function() {
